51badf3e40613753e04b52ab33208f4bcb31e54a
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-07-18 00:23:49 +0600

    added logic for starting servers

--- cmd/server.go
+++ cmd/server.go
@@ -14,9 +14,16 @@ type Handler struct {
 }
 
 func (h *Handler) StartServer() {
-	if ! h.config.EnableTCP {
+	if !h.config.EnableTCP {
 		log.Fatalf("TCP server is not enabled in config")
 	}
 
-	
+	tcpServer = &dns.Server{
+		Addr: ":853",
+	}
+
+	go tcpServer.ListenAndServe()
+	log.Printf("Started TCP server on port :853\n")
 }
+
+
